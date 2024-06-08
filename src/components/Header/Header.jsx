276777fcbb4fe31eadb4985f9e337fd171641ee8
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-08 22:58:17 +0530

    third-commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className='header'>
         <div className='header-content'>
-            <h2>Order the Food</h2>
-            <p>Choose from a diverse menu featuring array of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring array of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p><br/>
             <button>View Menu</button>
         </div>
     </div>
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Menu.css'
+import { menu_list } from '../../assets/assets'
+const Menu = ({category,setCategory}) => {
+  return (
+    <div className='Menu' id="Menu">
+        <h1>Our Menu</h1>
+        <p className='menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time.</p>
+        <div className='menu-list'>
+            {menu_list.map((item,index)=>{
+                return(
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='list-item'>
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=""/>
+                        <p>{item.menu_name}</p>
+                    </div>
+                )
+            })}
+        </div>
+        <hr/>
+    </div>
+  )
+}
+
+export default Menu
