efa7034717d872fbce68444117f3abf7dcf3c6ff
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-18 20:48:52 +0530

    5-commit

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -33,12 +33,12 @@ const PlaceOrder = () => {
           <hr/>
           <div className="cart-total-details">
             <p>Delivery Fee</p>
-            <p>Rs.{2}</p>
+            <p>Rs.{getTotalCartAmount()===0?0:2}</p>
           </div>
           <hr/>
           <div className="cart-total-details">
             <b>Total</b>
-            <b>Rs.{getTotalCartAmount()+2}</b>
+            <b>Rs.{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
           </div>
           <button >Proceed To Payment</button>
         </div>
