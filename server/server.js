1e1b4cc83bdaec58885fd2ce81bf9747fb4ee2ca
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-22 22:45:11 +0530

    2-commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import connectDB from './helper/db.js'
+import foodRouter from './routers/foodRoute.js'
 
 //app config
 const app=express()
@@ -12,6 +13,9 @@ app.use(cors())
 
 //db connection
 connectDB();
+
+//api endpoints
+app.use('./api/food',foodRouter)
 
 app.get("/",(req,res)=>{
     res.send("Api Working")
