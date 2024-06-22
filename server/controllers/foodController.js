266ccb8b8693059d3f1ccbdbd1381d2e9f5777ba
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-22 23:03:20 +0530

    3-commit

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -1,8 +1,23 @@
-import foodModel from '../models/foodmodel.js'
+import foodModel from '../models/foodModel.js'
 import fs from 'fs'
 
 //add food item
 const addFood = async(req,res)=>{
 
+    let image_filename = `${req.file.filename}`;
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+    })
+    try{
+        await food.save();
+        res.json({success:true,message:"Food Added"})
+    } catch(err){
+        console.log(err)
+        res.json({success:false,message:"Error"})
+    }
 }
 export {addFood}
