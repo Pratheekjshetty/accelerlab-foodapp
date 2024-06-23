f5e7dfa47db760daa7e2fba825d9846621eed6de
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-23 13:18:15 +0530

    7-commit

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Order from './pages/Orders/Order';
 
 function App() {
   return (
@@ -9,6 +13,11 @@ function App() {
       <Navbar/>
       <div className='app-content'>
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Order/>}/>
+        </Routes>
       </div> 
     </div>
   );
