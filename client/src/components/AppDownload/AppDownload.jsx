2a1639649669486cf83ade9556fd367deb531279
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-27 21:59:26 +0530

    1-commit

--- client/src/components/AppDownload/AppDownload.jsx
+++ client/src/components/AppDownload/AppDownload.jsx
@@ -6,6 +6,7 @@ const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
         <p>For Better Experience Download <br/> Festly App</p>
+        <p>Download our app using Playstore or Appstore</p>
         <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />&nbsp;
             <img src={assets.app_store} alt="" />
