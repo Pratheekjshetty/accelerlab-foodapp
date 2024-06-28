09aa602b2b9886f16b66b85e95148f87004c95f5
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-29 00:27:14 +0530

    7-commit

--- server/controllers/cartControllers.js
+++ server/controllers/cartControllers.js
@@ -25,7 +25,7 @@ const removeFromCart =async(req,res)=>{
     try{
         let userData = await userModel.findById(req.body.userId);
         let cartData = await userData.cartData;
-        if(cartData[req.body.itemId>0]){
+        if(cartData[req.body.itemId] > 0){
             cartData[req.body.itemId] -= 1;
         }
         await userModel.findByIdAndUpdate(req.body.userId,{cartData});
