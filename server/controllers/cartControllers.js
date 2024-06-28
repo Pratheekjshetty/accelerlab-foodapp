c1784339d1cf85132f4cedb7e5d74c243c5febe7
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-29 00:08:20 +0530

    6-commit

--- server/controllers/cartControllers.js
+++ server/controllers/cartControllers.js
@@ -23,10 +23,17 @@ const addToCart =async(req,res)=>{
 //remove items from user cart
 const removeFromCart =async(req,res)=>{
     try{
-        let userData = await userModel.findById(req.body.userId)
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if(cartData[req.body.itemId>0]){
+            cartData[req.body.itemId] -= 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+        res.json({success:true,message:"Removed From Cart"})
     }
     catch(err){
-
+        console.log(err);
+        res.json({success:false,message:"Error"})
     }
 
 }
