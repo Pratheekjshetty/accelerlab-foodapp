e623f917580aca694d314fde4b535a5ef01e60d3
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-29 09:03:26 +0530

    remove_from_cart

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -20,8 +20,11 @@ const StoreContextProvider = (props)=>{
             await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
         }
     }
-    const removeFromCart =(itemId)=>{
+    const removeFromCart =async (itemId)=>{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+            if(token){
+                await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+            }
         }
     // useEffect(()=>{
     //     console.log(cartItems);
