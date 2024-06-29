11e2b43020340c6f933c0c23c831768178847be1
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-29 22:04:54 +0530

    orderControllers

---
+++ server/controllers/orderControllers.js
@@ -0,0 +1,10 @@
+import orderModel from "../models/orderModels.js";
+import userModel from "../models/userModels.js";
+import stripe from 'stripe'
+
+//placing user order for frontend
+const placeOrder = async (req,res)=>{
+
+}
+
+export {placeOrder}
