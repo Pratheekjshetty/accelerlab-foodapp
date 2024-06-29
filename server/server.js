c06a3cd2d67bb6ea783a7c06cfb099c325a83999
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-29 22:42:57 +0530

    stripe_key

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import foodRouter from './routers/foodRoutes.js'
 import userRouter from './routers/userRoutes.js'
 import 'dotenv/config'
 import cartRouter from './routers/cartRoutes.js'
+import orderRouter from './routers/orderRoutes.js'
 
 //app config
 const app=express()
@@ -22,6 +23,7 @@ app.use('/api/food',foodRouter)
 app.use('/images',express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
     res.send("Api Working")
