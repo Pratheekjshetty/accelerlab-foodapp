5484b78b08f56bca634253a740fb14a0b66c6c1a
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-07-03 20:14:06 +0530

    myorders updated2

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import Login from './components/LoginPopup/Login';
 import Verify from './pages/Verify/Verify';
+import MyOrders from './pages/MyOrders/MyOrders';
 
 function App() {
   const[showLogin,setShowLogin] = useState(false);
@@ -21,6 +22,7 @@ function App() {
         <Route path='/cart'element={<Cart/>}/>
         <Route path='/order'element={<PlaceOrder/>}/>
         <Route path='/verify'element={<Verify/>}/>
+        <Route path='/myorders'element={<MyOrders/>}/>
       </Routes>   
     </div>
     <Footer/>
--- client/src/pages/MyOrders/MyOrders.jsx
+++ client/src/pages/MyOrders/MyOrders.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './MyOrders.css'
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
+import { assets } from '../../assets/assets';
 
 const MyOrders = () => {
+    const {url,token} = useContext(StoreContext);
+    const [data,setData] = useState([]);
+
+    useEffect(()=>{
+    const fetchOrders = async ()=>{
+        try{
+            const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}});
+            setData(response.data.data);
+        }
+        catch(err){
+            console.log(err);
+        }    
+    };
+        if(token && url){
+            fetchOrders();
+        }
+    },[token,url]);
   return (
-    <div>MyOrders</div>
+    <div className='my-orders'>
+        <h2>My Orders</h2>
+        <div className="container">
+            {data.map((order,index)=>{
+                return(
+                    <div key={index} className='my-orders-order'>
+                        <img src={assets.parcel_icon} alt=""/>
+                        <p>{order.items.map((item,index)=>{
+                            if(index === order.items.length-1){
+                                return item.name +" x "+ item.quantity
+                            }
+                            else{
+                                return item.name +" x "+ item.quantity +", "
+                            }
+                        })}</p>
+                        <p>Rs.{order.amount}.00</p>
+                        <p>Items:{order.items.length}</p>
+                        <p><span>&#x25cf;</span> <b>{order.status}</b></p>
+                        <button>Track Order</button>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
